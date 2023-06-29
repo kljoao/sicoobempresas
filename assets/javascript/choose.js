bc036c17efc5850c475f1fb954d1287bdf646374
João Ramos <joao.ramos@infocrerj.local>
2023-06-29 18:01:25 -0300

    choose-1

---
+++ assets/javascript/choose.js
@@ -0,0 +1,7 @@
+const welcomeBtn = document.querySelector('#welcomeBtn');
+const upWelcome = document.querySelector('.upWelcome');
+const downWelcome = document.querySelector('.downWelcome');
+
+function dropWelcome(){
+    welcomeBtn.classList.toggle('ativoW');
+}
