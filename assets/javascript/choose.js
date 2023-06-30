84e7035b2624094650fc32aae17167cf0219dc8c
João Ramos <joao.ramos@infocrerj.local>
2023-06-30 16:25:51 -0300

    choose-ramal

--- assets/javascript/choose.js
+++ assets/javascript/choose.js
@@ -1,7 +1,12 @@
+const welcomeAll = document.querySelector('.choose-user-container')
 const welcomeBtn = document.querySelector('#welcomeBtn');
 const upWelcome = document.querySelector('.upWelcome');
 const downWelcome = document.querySelector('.downWelcome');
 
 function dropWelcome(){
     welcomeBtn.classList.toggle('ativoW');
+}
+
+function welcomeAllF(){
+    welcomeAll.classList.toggle('ativoW');
 }
